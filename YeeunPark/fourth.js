53195446309a9b7a2206fe16349be639fe04f748
yeeun park <145003970+yengniws@users.noreply.github.com>
2024-04-07 20:00:02 +0900

    Feat:second done

--- YeeunPark/fourth.js
+++ YeeunPark/fourth.js
@@ -7,15 +7,18 @@
 // schedule["8:30"] = "get up";
 // alert( isEmpty(schedule) ); // false
 
-let schedule = {};
-console.log(isEmpty(schedule));
-
-schedule["8:30"] = "get up";
-console.log(isEmpty(schedule));
-
 function isEmpty(obj) {
     for (let i in obj) {
         return false;
     }
     return true;
 }
+
+let schedule = {};
+console.log(isEmpty(schedule));
+
+schedule["8:30"] = "get up";
+console.log(isEmpty(schedule));
+
+schedule["test"] = null;
+console.log(isEmpty(schedule));
