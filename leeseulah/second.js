07d75cdd3da5e35c47c61dd45d7f00d8fd5186af
seulah03 <164130209+seulah03@users.noreply.github.com>
2024-04-08 23:23:54 +0900

    Update second.js

--- leeseulah/second.js
+++ leeseulah/second.js
@@ -2,7 +2,7 @@ function sumInput() {
     let numArray = new Array();
     while (true) {
         let num = prompt("숫자를 입력하세요");
-        if (isNaN(num) || num == "" || num == null) {
+        if (isNaN(num) || num == "" || num == "" || num == null) {
             break;
         }
         numArray.push(num);
